bcae3b8b5e994eda22c05e0bff699e7ce5f083b8
Akil-GH <139623503+Akil-GH@users.noreply.github.com>
2024-03-01 16:30:45 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import Meme from "./components/Meme";
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Meme />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <small>
+          Coded  by{" "}
+          <a
+            className="footer--link"
+            href="https://www.linkedin.com/in/akildesai/"
+            target="_blank"
+            rel="noreferrer"
+          >
+           Akil Desai
+          </a>
+          .
+        </small>
+      </footer>
+    );
+  }
